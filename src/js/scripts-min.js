3a0f80c91f319c78bf51928402413d70a62cc555
Gourman <carlosvicente76@gmail.com>
2021-03-26 10:04:52 +0100

    animation background

--- src/js/scripts-min.js
+++ src/js/scripts-min.js
@@ -1,23 +1,51 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const panelBtn = document.querySelector('.panel-btn')
-//     if (panelBtn) {
-//         panelBtn.addEventListener('click', () => {
-//             const panel = document.querySelector('.panel')
-//             panel.classList.toggle('is-active')
-//             panelBtn.classList.toggle('is-active')
-//         })
+
+
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const menu = document.getElementsByClassName('menu')[0]
+
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+// cambiar el body de color
+// function changeBackground() {
+//     if (window.scrollY > window.innerHeight / 2) {
+//         document.body.classList.add('bg-active');
+//     } else {
+//         document.body.classList.remove('bg-color')
 //     }
-//     const menuLink = document.querySelector('.menu__link')
-//     if (menuLink) {
-//         menuLink.addEventListener('click', () => {
+// }
 
-//             panel.classList.remove('is-active')
-//             panelBtn.classList.remove('is-active')
-//         })
+// function changeBackground() {
+//     if (window.scrollY > window.innerHeight / 2) {
+//         document.body.classList.add('bg-active');
 //     }
-// })
+//     else {
+//         document.body.classList.remove('bg-active')
+
+//     }
+// }
+
+// window.addEventListener('scroll', changeBackground);
 
 
 
 
+gsap.utils.toArray(".panel").forEach(function (elem) {
 
+    var color = elem.getAttribute('data-color');
+
+    ScrollTrigger.create({
+        trigger: elem,
+        start: 'top 50%',
+        end: 'bottom 50%',
+        onEnter: () => gsap.to('main', { backgroundColor: color }),
+        onLeave: () => gsap.to('main', { backgroundColor: 'white' }),
+        onLeaveBack: () => gsap.to('main', { backgroundColor: 'white' }),
+        onEnterBack: () => gsap.to('main', { backgroundColor: color }),
+        // markers: true
+    });
+
+});
